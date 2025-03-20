6ebc0f0cedba794b6f131d37fffa2b0b7402eec8
Krishanu Mishra <krishanu.mishra@intimetec.com>
2025-03-20 19:15:03 +0530

    Initial Commit

--- lib/code-build-stack.ts
+++ lib/code-build-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CodeBuildStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const vpc = new ec2.Vpc(this, 'Vpc');
+        const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+        const repository = ecr.Repository.fromRepositoryName(this, 'EcrRepo', 'err-repo');
+        const image = ecs.ContainerImage.fromEcrRepository(repository,'latest');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CodeBuildQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef');
+        taskDefinition.addContainer('Container', {
+            image: ecs.ContainerImage.fromEcrRepository(repository),
+            memoryLimitMiB: 512,
+            cpu: 256
+        });
+
+        const service = new ecs.FargateService(this, 'ecs-service',{
+          cluster,
+          taskDefinition,
+          desiredCount:0
+        })
+
+        new lambda.Function(this, 'LambdaFunction', {
+            runtime: lambda.Runtime.NODEJS_18_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'index.handler'
+        });
+        new s3.Bucket(this,'Bucket',{
+          bucketName: 'bucket-name',
+          removalPolicy: cdk.RemovalPolicy.DESTROY
+        })
+    }
 }
